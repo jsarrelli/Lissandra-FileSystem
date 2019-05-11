460a0467b4fe316a9d6fd30486d20645f9b6d466
jsarrelli <julisarrelli@gmail.com>
2019-05-11 14:47:33 -0300

    insert paginamemoria

--- Memoria/src/MemoriaPrincipal.c
+++ Memoria/src/MemoriaPrincipal.c
@@ -3,7 +3,8 @@
 void inicializarEstadoMemoria() {
 	estadoMemoria=list_create();
 	for (int i = 0; i < cantPaginas - 1; i++) {
-		list_add() = LIBRE;
+		list_add(estadoMemoria,LIBRE);
+		//consultar
 	}
 }
 
@@ -15,27 +16,53 @@ void inicializarMemoria(int valueMaximoRecibido){
 	inicializarEstadoMemoria();
 }
 
-Segmento insertarSegmento(char* nombreSegmento){
-	Segmento segmento={nombreSegmento, "", list_create(),0};
+void insertarSegmento(char* nombreSegmento){
+
+	Segmento* segmento=malloc(sizeof(Segmento));
+	strcpy(segmento->nombreTabla,nombreSegmento);
+	segmento->nombreModificado=1;
+	segmento->paginas=list_create();
 	list_add(segmentos,segmento);
-	return segmento;
+	free(segmento);
+
 
 }
 
-//Pagina insertarPagina(int key, char* value){
-//	Pagina pagina=NULL;
-//		if(!existePagina()&& memoriaLlena()){
-//
-//		}
-//	return pagina;
-//}
+//inserta una pagina en la memoria y te devuelve la direccion de
+//donde la puso
+Pagina* insertarPagina(int key, char* value, char* nombreSegmento){
+	Segmento* segmento=buscarSegmento(nombreSegmento);
+	Pagina* paginaNueva=NULL;
+		if(existeSegmento(nombreSegmento) && !existePagina(segmento,key) && !memoriaLlena()){
+			void* marcoVacio = darMarcoVacio();
+
+			t_registro registro={.key=key, .value=value};
+			t_registro* registroEnMemoria=memcpy(marcoVacio,&registro,sizeof(t_registro));
+
+			paginaNueva = malloc(sizeof(Pagina));
+			paginaNueva->modificado=0;
+			paginaNueva->registro=registroEnMemoria;
+
+			list_add(segmento->paginas,paginaNueva);
+		}
+	return paginaNueva;
+}
+
+void* darMarcoVacio(){
+	for(int i=0; i<list_size(estadoMemoria);i++){
+		if((int)list_get(estadoMemoria,i) == LIBRE){
+			return memoria+i*sizeof(t_registro);
+		}
+	}
+	return NULL;
+}
 
 
-Pagina buscarPagina(Segmento segmento,int key){
-	if(existeSegmento(segmento)){
-		for(int i=0;i<list_size(segmento.paginas)-1;i++){
-			Pagina paginaActual=(Pagina) list_get(segmento.paginas,i);
-			if(paginaActual.registro->key==key){
+Pagina* buscarPagina(Segmento* segmento,int key){
+	if(existeSegmento(segmento->nombreTabla)){
+		for(int i=0;i<list_size(segmento->paginas)-1;i++){
+			Pagina* paginaActual=(Pagina*) list_get(segmento->paginas,i);
+			if(paginaActual->registro->key==key){
 				return paginaActual;
 			}
 		}
@@ -43,27 +70,27 @@ Pagina buscarPagina(Segmento segmento,int key){
 	return NULL;
 }
 
-Segmento buscarSegmento(char* nombreSegmento){
+Segmento* buscarSegmento(char* nombreSegmento){
 	for(int i=0;i<list_size(segmentos);i++){
-		Segmento segmentoActual=(Segmento)list_get(segmentos,i);
-		if(strcmp(segmentoActual.nombreTabla,nombreSegmento)){
+		Segmento* segmentoActual=(Segmento*)list_get(segmentos,i);
+		if(strcmp(segmentoActual->nombreTabla,nombreSegmento)){
 			return segmentoActual;
 		}
 	}
 	return NULL;
 }
 
-bool existeSegmento(Segmento segmento){
-	return buscarSegmento(segmento.nombreTabla)!=NULL;
+bool existeSegmento(char* nombreSegmento){
+	return buscarSegmento(nombreSegmento)!=NULL;
 }
 
-bool existePagina(Segmento segmento,int key){
+bool existePagina(Segmento* segmento,int key){
 	return buscarPagina(segmento,key)!=NULL;
 }
 
 bool memoriaLlena(){
-	t_list espaciosDisponibles=list_filter(estadoMemoria,estaLibre);
-	return list_size(espaciosDisponibles)>0;
+	t_list *espaciosDisponibles=list_filter(estadoMemoria,LIBRE);
+	return list_size(espaciosDisponibles)==0;
 }
 
 bool estaLibre(int estado){
