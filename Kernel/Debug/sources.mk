cef871c3fe72baff81b37cfc7bada2895a61232f
GonzaOrtega <gonzalonortega@gmail.com>
2019-06-04 18:25:00 -0300

    Ahora si creo que arreglo el problema de la compilacion por consola del Kernel

---
+++ Kernel/Debug/sources.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Libraries/Debug \
+Libraries/Debug/commons \
+Libraries/Debug/commons/collections \
+Libraries \
+Libraries/commons \
+Libraries/commons/collections \
+src \
+
---
+++ LissandraFileSystem/Debug/sources.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Libraries2/Debug \
+Libraries2/Debug/commons \
+Libraries2/Debug/commons/collections \
+Libraries2 \
+Libraries2/commons \
+Libraries2/commons/collections \
+src \
+
