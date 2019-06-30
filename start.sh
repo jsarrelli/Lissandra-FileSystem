3a05ab08f1623a4598644bb1921b3bdb499cbf68
erratchumatias <erratchumatias@gmail.com>
2019-06-30 05:41:44 -0300

    Mejora start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-1c-Los-Sisoperadores/Libraries/Debug/
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-1c-Los-Sisoperadores/Libraries/Debug/' >> ~/.bashrc 
