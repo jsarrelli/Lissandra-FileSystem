243d97b48525ca79af7156f700add8b1fe941acd
Julian <jsarrelli@gmail.com>
2019-07-09 21:39:38 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-1c-Los-Sisoperadores/Libraries/Debug/' >> ~/.bashrc 
 
+echo 'Bajando commons..'
 git clone https://github.com/sisoputnfrba/so-commons-library.git
 cd so-commons-library/
 sudo make install
 cd
 
+echo 'Compilando Libraries'
 cd tp-2019-1c-Los-Sisoperadores/Libraries/Debug/
 make clean
 make 
 cd 
 
+echo 'Compilando Kernel'
 cd tp-2019-1c-Los-Sisoperadores/KernelV2/Debug/
 make clean
 make
 cd 
 
+echo 'Compilando Memoria'
 cd tp-2019-1c-Los-Sisoperadores/Memoria/Debug/
 make clean
 make
 cd 
 
+echo 'Compilando Filesystem'
 cd tp-2019-1c-Los-Sisoperadores/Memoria/Debug/
 make clean
 make
