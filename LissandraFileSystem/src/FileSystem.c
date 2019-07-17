70cf419ee29050063bfd50f12c71074445b94241
Julian <jsarrelli@gmail.com>
2019-07-17 09:18:26 -0300

    Para correr

--- LissandraFileSystem/src/FileSystem.c
+++ LissandraFileSystem/src/FileSystem.c
@@ -122,7 +122,7 @@ int cargarMetadata() {
 	string_append(&path, "/Metadata.bin");
 	rutas.Metadata = path;
 	FILE*arch1 = fopen(rutas.Metadata, "w+");
-	fprintf(arch1, "BLOCK_SIZE=30\n");
+	fprintf(arch1, "BLOCK_SIZE=100\n");
 	fprintf(arch1, "BLOCKS=3000\n");
 	fprintf(arch1, "MAGIC_NUMBER=LISSANDRA\n");
 	fclose(arch1);
