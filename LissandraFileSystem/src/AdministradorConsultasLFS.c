84d0af9f003482eec4ca7e56fc66aaac1d7ee7e0
kevdorfman <kevdorfman95@gmail.com>
2019-07-20 03:03:50 -0300

    cambio

--- LissandraFileSystem/src/AdministradorConsultasLFS.c
+++ LissandraFileSystem/src/AdministradorConsultasLFS.c
@@ -8,7 +8,7 @@ int funcionCREATE(char* nombreTabla, char* cantParticiones, char* consistenciaCh
 	crearTablaYParticiones(nombreTabla, cantParticiones);
 	crearMetadataTabla(nombreTabla, consistenciaChar, cantParticiones, tiempoCompactacion);
 	pthread_t threadCompactacion;
-	pthread_create(&threadCompactacion, NULL, (void*) iniciarThreadCompactacion, nombreTabla);
+	pthread_create(&threadCompactacion, NULL, (void*) iniciarThreadCompactacion, (void*)nombreTabla);
 	pthread_detach(threadCompactacion);
 	return 0;
 }
